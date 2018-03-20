f70c59ced1c150167c01b1e7e7cb989d90fe1887
orlanth23 <orlanth23@hotmail.com>
2018-03-20 22:58:14 +1100

    Cool stuff again

--- app/src/main/java/oliweb/nc/oliweb/ui/fragment/AnnonceDetailActivity.java
+++ app/src/main/java/oliweb/nc/oliweb/ui/fragment/AnnonceDetailActivity.java
@@ -3,10 +3,11 @@ package oliweb.nc.oliweb.ui.fragment;
 
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,14 +26,15 @@ public class AnnonceDetailActivity extends AppCompatActivity {
     @BindView(R.id.view_pager_detail)
     ViewPager viewPager;
 
-    @BindView(R.id.default_button)
-    FloatingActionButton fabDefaultButton;
-
     @BindView(R.id.indicator_detail)
     CircleIndicator indicator;
 
     @BindView(R.id.text_description_detail)
     TextView description;
+
+    @BindView(R.id.text_view_prix_detail)
+    TextView prix;
+
 
     private AnnoncePhotos annoncePhotos;
 
@@ -53,6 +55,7 @@ public class AnnonceDetailActivity extends AppCompatActivity {
         }
 
         if (annoncePhotos != null) {
+            prix.setText(String.valueOf(String.format(Locale.FRANCE,"%,d", annoncePhotos.getAnnonceEntity().getPrix()) + " XPF"));
             description.setText(annoncePhotos.getAnnonceEntity().getDescription());
             collapsingToolbarLayout.setTitle(annoncePhotos.getAnnonceEntity().getTitre());
             if (annoncePhotos.getPhotos() != null && !annoncePhotos.getPhotos().isEmpty()) {
