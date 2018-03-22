eff8b89d69213a123218e8988d26d1716b06c94b
2761oli <jean-paul.olive@opt.nc>
2018-03-22 16:43:05 +1100

    Feat : Début d'implémentation des notifications

--- app/src/main/java/oliweb/nc/oliweb/service/MyFirebaseMessagingService.java
+++ app/src/main/java/oliweb/nc/oliweb/service/MyFirebaseMessagingService.java
@@ -1,10 +1,17 @@
 package oliweb.nc.oliweb.service;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * Created by 2761oli on 22/03/2018.
  */
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO overrider cette méthode pour faire apparaître clairement les notifications.
+        super.onMessageReceived(remoteMessage);
+    }
 }
