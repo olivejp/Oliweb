055716eb556c6f242a501136684c9ffe3a8f636b
orlanth23 <orlanth23@hotmail.com>
2018-04-02 07:18:58 +1100

    Feat : Change DaetFormater for the adapter raw and change the overridePendingTransition for the PostAnnonceActivity and the MyAnnonceActivity

--- app/src/main/java/oliweb/nc/oliweb/ui/activity/MyChatsActivity.java
+++ app/src/main/java/oliweb/nc/oliweb/ui/activity/MyChatsActivity.java
@@ -60,6 +60,7 @@ public class MyChatsActivity extends AppCompatActivity {
             getSupportFragmentManager().popBackStack();
         } else {
             super.onBackPressed();
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
         }
     }
 }
