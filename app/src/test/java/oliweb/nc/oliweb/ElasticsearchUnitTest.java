9a0cec9488661a564a3761ee00f9efd2f20fb828
orlanth23 <orlanth23@hotmail.com>
2018-09-24 09:50:01 +1100

    Test : Modification du test ElasticsearchAueryBuilder avec les modifications de la recherche avancée.

--- app/src/test/java/oliweb/nc/oliweb/ElasticsearchUnitTest.java
+++ app/src/test/java/oliweb/nc/oliweb/ElasticsearchUnitTest.java
@@ -1,11 +1,18 @@
 package oliweb.nc.oliweb;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -21,23 +28,47 @@ import static junit.framework.TestCase.fail;
 @RunWith(JUnit4.class)
 public class ElasticsearchUnitTest {
 
+    private Gson gson;
+
+    @Before
+    public void setUp(){
+        gson = new Gson();
+    }
+
     @Test
     public void test_elasticsearch_query_builder() {
-        ElasticsearchQueryBuilder elasticsearchQueryBuilder = new ElasticsearchQueryBuilder();
-        elasticsearchQueryBuilder.setFrom(2);
-        elasticsearchQueryBuilder.setSize(10);
+        File json = new File(getClass().getResource("/elasticsearch.json").getPath());
+        JsonElement jsonElement = null;
+        try {
+            JsonParser jsonParser = new JsonParser();
+            jsonElement = jsonParser.parse(new FileReader(json));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        List<String> listCategorie = new ArrayList<>();
+        listCategorie.add("Immobilier");
+        listCategorie.add("Automobile");
+        listCategorie.add("Meuble");
 
         List<String> listFields = new ArrayList<>();
         listFields.add("titre");
         listFields.add("description");
 
+        ElasticsearchQueryBuilder elasticsearchQueryBuilder = new ElasticsearchQueryBuilder();
+        elasticsearchQueryBuilder.setFrom(2);
+        elasticsearchQueryBuilder.setSize(10);
+        elasticsearchQueryBuilder.setCategorie(listCategorie);
+        elasticsearchQueryBuilder.setRangePrice(1000, 2000);
+        elasticsearchQueryBuilder.setWithPhotoOnly();
         elasticsearchQueryBuilder.setMultiMatchQuery(listFields, "recherche");
-
         elasticsearchQueryBuilder.addSortingFields("datePublication", "ASC");
 
         JsonObject valueReturned = elasticsearchQueryBuilder.build();
-        assertNotNull(valueReturned);
-        assertEquals("{\"from\":2,\"size\":10,\"query\":{\"multi_match\":{\"query\":\"recherche\",\"fields\":[\"titre\",\"description\"]}},\"sort\":[{\"datePublication\":{\"order\":\"ASC\"}}]}", valueReturned.getAsString());
+        String requestJson = gson.toJson(valueReturned);
+        String correctJson = gson.toJson(jsonElement);
+        assertNotNull(requestJson);
+        assertEquals(correctJson, requestJson);
     }
 
     @Test
