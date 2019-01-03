ae4c83203b18047dff45ba3e0bec9e10df0d6b99
2761oli <jean-paul.olive@opt.nc>
2019-01-03 16:13:01 +1100

    fix: MainActivityTest now passed.

--- app/src/androidTest/java/oliweb/nc/oliweb/activity/MainActivityTest.java
+++ app/src/androidTest/java/oliweb/nc/oliweb/activity/MainActivityTest.java
@@ -1,6 +1,7 @@
 package oliweb.nc.oliweb.activity;
 
 import android.content.Intent;
+import android.util.Log;
 
 import org.junit.After;
 import org.junit.Before;
@@ -9,6 +10,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import androidx.lifecycle.ViewModelProviders;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import oliweb.nc.oliweb.R;
@@ -16,9 +19,11 @@ import oliweb.nc.oliweb.ui.activity.MainActivity;
 import oliweb.nc.oliweb.ui.activity.viewmodel.MainActivityViewModel;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static oliweb.nc.oliweb.UtilityTest.UID_USER;
 import static oliweb.nc.oliweb.ui.activity.ProfilActivity.PROFIL_ACTIVITY_UID_USER;
 import static oliweb.nc.oliweb.ui.activity.ProfilActivity.UPDATE;
@@ -31,6 +36,7 @@ import static oliweb.nc.oliweb.ui.activity.ProfilActivity.UPDATE;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
+    private static final String TAG = MainActivityTest.class.getCanonicalName();
     private MainActivityViewModel viewModel;
 
     @Rule
@@ -49,8 +55,21 @@ public class MainActivityTest {
     }
 
     @Test
-    public void saveUserTest() {
-        // Vérification que le profil de l'utilisateur apparaît correctement
+    public void openDrawerLayout() throws InterruptedException {
+
+        Thread.sleep(2000L);
+
+        // On ferme la fenêtre qui demande de récupérer les annonces déjà existantes
+        try {
+            onView(withText("NON")).perform(click());
+        } catch (NoMatchingViewException e) {
+            Log.e(TAG, e.getLocalizedMessage(), e);
+        }
+
+        // On tente d'ouvrir le drawer
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+
+        // Le drawer devrait s'ouvrir
         onView(withId(R.id.nav_view)).check(matches(isDisplayed()));
     }
 
